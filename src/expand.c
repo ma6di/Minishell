c1ddbf6cc700b0fdebee29dc8c28dd401aa4d23e
Natalie Rauh <natalierauh2@gmail.com>
2024-11-05 13:48:26 -0600

    wip implement expansion

--- src/expand.c
+++ src/expand.c
@@ -71,10 +71,8 @@ char	**get_keys(char **env_keys, int keys, char *str)
 	return (filtered_envp);
 }*/
 
-/*char	*expand_values(char **env_keys, char **envp, int key_count)
+char	*expand_values(char **env_keys, char ***envp_key_val, int key_count, t_token token)
 {
-	// search the item in envp
-	char	***filtered_envp;
 
 	// receiving the current token so that the old value can be freed....
 	filtered_envp = filter_envp(envp, env_keys, key_count);
@@ -84,7 +82,8 @@ char	**get_keys(char **env_keys, int keys, char *str)
 	// free the value
 	// store joined str in the value
 	// free previous curr->value
-	return (NULL);
+	free(token->value);
+	return (joined);
 }*/
 
 char	***split_key_value(char **envp)
@@ -112,28 +111,27 @@ char	***split_key_value(char **envp)
 
 t_token	**expand(t_token **head, char **envp)
 {
-	//t_token	*curr;
+	t_token	*curr;
 	char	***envp_key_val;
-	//char	**env_keys;
-	//int		key_count;
+	char	**env_keys;
+	int		key_count;
 
 	envp_key_val = split_key_value(envp);
 	if (!envp_key_val)
 		return (free_key_val(envp_key_val), NULL);
 	print_key_val(envp_key_val);
 	free_key_val(envp_key_val);
-	/*curr = *head;
+	*curr = *head;
 	while (curr)
 	{
 		if (curr->state != STATE_QUOTE)
 		{
 			key_count = count_vars(curr->value);
-			printf("keys: %d\n", key_count);
 			env_keys = malloc((key_count + 1) * sizeof(char *));
 			env_keys[key_count] = NULL;
 			env_keys = get_keys(env_keys, key_count, curr->value);
 			// should return updated list...
-			//value = expand_values(env_keys, envp, key_count);
+			curr->value = expand_values(env_keys, envp, key_count, curr);
 			//printf("%s\n", value);
 			print_keys(env_keys);
 			free_keys(env_keys);
